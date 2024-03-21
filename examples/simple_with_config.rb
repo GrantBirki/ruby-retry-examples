5c11404b7f376c89d510ab8923dfcb3005b1799e
GrantBirki <grantbirki@github.com>
2024-03-21 10:52:14 -0600

    update config

--- examples/simple_with_config.rb
+++ examples/simple_with_config.rb
@@ -2,20 +2,23 @@
 
 require "retryable"
 
-Retryable.configure do |config|
-  config.contexts     = {}
-  config.ensure       = proc {}
-  config.exception_cb = proc {}
-  config.log_method   = proc {}
-  config.matching     = /.*/
-  config.not          = []
-  config.on           = StandardError
-  config.sleep        = 1
-  config.sleep_method = ->(n) { Kernel.sleep(n) }
-  config.tries        = 2
-end
+# create a new configuration object
+config = Retryable::Configuration.new
 
-Retryable.retryable do
+# set the configuration options
+config.contexts     = {}
+config.ensure       = proc {}
+config.exception_cb = proc {}
+config.log_method   = proc {}
+config.matching     = /.*/
+config.not          = []
+config.on           = StandardError
+config.sleep        = 1
+config.sleep_method = ->(n) { Kernel.sleep(n) }
+config.tries        = 4
+
+# use the configuration object in the retryable block
+Retryable.retryable(config) do
   puts "Hello, world!"
   raise StandardError, "This is an error"
 end
